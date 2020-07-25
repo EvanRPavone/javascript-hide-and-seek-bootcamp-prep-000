2e5f6e1ff0f2fc86bee00479f1eabe4c5d60c38e
Evan Pavone <evanrpavone@gmail.com>
2020-07-25 13:48:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,26 +1,29 @@
-function getFirstSelector(selector) {
-  return document.querySelector(selector);
+var selectorValue="main.#app li";
+function getFirstSelector(selectorValue){
+return document.querySelector(selectorValue);
 }
 
-function nestedTarget() {
-  return document.querySelector('#nested .target');
+function nestedTarget(){
+  var element='div';
+  var divNested=document.getElementById('app').querySelector(`${element}#nested .target`);
+  return divNested;
 }
 
-
-function increaseRankBy(n) {
-  const list = document.quarySelectorAll('ul.ranked-list li')
-  for (var i = 0; i < list.length; i++) {
-      list[i].innerHTML = parseInt(list[i].innerHTML) + n;
-    }
+function increaseRankBy(n){
+ var ranked_list_value=document.querySelectorAll("ul.ranked-list li");
+ for(let i=0; i<ranked_list_value.length;i++){
+   console.log("ranked_list_value : " + ranked_list_value[i]);
+   console.log("ranked_list_value.innerText : " + ranked_list_value[i].innerHTML);
+   var value=parseInt(ranked_list_value[i].innerHTML) + n;
+   console.log("value : "+value)
+   ranked_list_value[i].innerHTML=value;
   }
+}
 
 function deepestChild() {
-  let gNode = document.querySelector('#grand-node')
-  let nextNode = gNode.children[0];
-
-  while (nextNode) {
-    gNode = nextNode;
-    nextNode = gNode.children[0];
+  var childNodeOfDiv=document.getElementById('app').querySelector('div#grand-node');
+  while(childNodeOfDiv.children[0]){
+    childNodeOfDiv = childNodeOfDiv.children[0]
   }
-  return (gNode);
+  return childNodeOfDiv;
 }
